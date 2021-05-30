659a09e4ff17c7774b90a3bc7f2e0d29ac25be81
Mati Hagen Esper <mahagen@uc.cl>
2021-05-30 17:28:47 -0400

    Se seleccionan los valores del Input mediante jquery, se integra con API y asignan variables para el contenido del JSON de la API

---
+++ assets/js/index.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+
+    $("form").submit(function(e){
+        e.preventDefault();
+
+        let numeroDeSuperHero = $("#numeroDeSuperHero").val();
+        alert(numeroDeSuperHero);
+
+        $.ajax({
+            url: "https://www.superheroapi.com/api.php/10223882662909856/" + numeroDeSuperHero,
+            success: function(data) {
+
+                /* Datos Generales del SuperHero */
+
+                let {name: nombre} = data;
+                let {url: imageURL} = data.image;
+                let {'group-affiliation': conexiones} = data.connections;
+                let {'publisher': publicadoPor} = data.biography;
+                let {'occupation': ocupacion} = data.work;
+                let {'first-appearance': primeraAparicion} = data.biography;
+                let {height: altura} = data.appearance;
+                let alturaJoin = altura.join(" - ")
+                let {weight: peso} = data.appearance;
+                let pesoJoin = peso.join(" - ")
+                let {aliases: alianzas} = data.biography;
+                let alianzasJoin = alianzas.join(", ");
+                
+                /* Stats del SuperHero */
+
+                let {'intelligence': inteligencia} = data.powerstats;
+                let {'strength': fuerza} = data.powerstats;
+                let {'speed': velocidad} = data.powerstats;
+                let {'durability': durabilidad} = data.powerstats;
+                let {'power': poder} = data.powerstats;
+                let {'combat': combate} = data.powerstats;
+
+                $("#generalData").html(`
+                <h3 class="text-center mb-3">SuperHero encontrado</h3>
+                <div class="card mb-3">
+                    <div class="row no-gutters">
+                      <div class="col-md-5">
+                        <img class="card-img-top" src="${imageURL}">
+                      </div>
+                      <div class="col-md-7">
+                        <div class="card-body">
+                          <h4 class="card-title text-center"><strong>${nombre}</strong></h4>
+                          <p class="card-text"><strong>Conexiones:</strong></br>
+                          ${conexiones}
+                          </p>
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Ocupación: ${ocupacion}</li>
+                            <li class="list-group-item">Primera Aparición: ${primeraAparicion}</li>
+                            <li class="list-group-item">Altura: ${alturaJoin}</li>
+                            <li class="list-group-item">Peso: ${pesoJoin}</li>
+                            <li class="list-group-item">Alianzas: ${alianzasJoin}</li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card-footer text-right">
+                        Publicado por: ${publicadoPor}
+                    </div>
+                </div>
+                `)
+
+                $("#grafico").html(`
+                
+                `)
+                
+            }
+        })
+
+    })
+
+})
